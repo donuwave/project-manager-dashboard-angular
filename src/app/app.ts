25c1f07b72897db4220933688197bb99da09a7a8
donuwave <d_tareev@mail.ru>
2025-11-08 20:42:22 +0300

    ref: изменена версия проекта

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import {Header} from './widgets/header/header';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule, InputTextModule, Header],
+  templateUrl: './app.html',
+  standalone: true,
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('project');
+}
